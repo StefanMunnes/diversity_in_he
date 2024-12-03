acb0c4940e0c19b492ee798475a84b1aedbcdbd7
Stefan Munnes <munnes@wzb.eu>
2024-12-03 15:13:16 +0100

    new keywords; extract just matching and surrounding text elements and export for inspection

--- topic_modeling/data_filter.py
+++ topic_modeling/data_filter.py
@@ -1,9 +1,12 @@
 import polars as pl
-import pandas as pd
+import re
 
 # 1. filter all text elements by diviersity keywords -> get unique urls
 # 2. keep all text elements from filteres urls
 # 3. keep just paragraphs (just for BERT relevant?)
+
+# TODO Check for Umlaute encoding, e.g. Diversity/Diversit√§t
+# https://www.bht-berlin.de/4422
 
 def print_data_info(df: pl.DataFrame) -> None:
     """
@@ -34,44 +37,104 @@ print_data_info(data_clean)
 
 # 1. filter polars dataframe text elements by diversity keywords
 
-# keywords from Sonia via Mail (2024.10.23 16:42)
+# keywords from meeting with Sonia and Yasemin (see mails from 2024.12.02 and 2024.12.03)
 keywords = [
-    "diversity", "inkluson", "inclusion", "discrimination", "equality", "equal opportunity", "gleichstellung"
-    # "divers",
-    # "gleichstellung",
-    # "equality",
-    # "equal opportunity",
-    # "equity",
-    # "inclus",
-    # "inklusion",
-    # "chancengleichheit",
-    # "frauen",
-    # "women",
-    # "gender",
-    # "geschlecht",
-    # "family",
-    # "familien"
+    "diversity",
+    "diversität",
+    "inclusion",
+    "inklusion",
+    "discrimination",
+    "diskriminierung",
+    "equality",
+    "gleichstellung",
+    "equal opportunity",
+    "chancengerechtigkeit",
+    "chancengleichheit"
 ]
 
-pattern = "|".join([f"(?i){keyword}" for keyword in keywords])
+pattern = r'\b(' + '|'.join(re.escape(word) for word in keywords) + r')\b'
 
-data_keywords = (
-    data_clean
-    .filter(pl.col("text").str.contains(pattern))
-    .filter(~pl.col("text").str.contains("(?i)biodivers"))
-    .filter(~pl.col("text").str.contains("(?i)private equity"))
-)
+# data_keywords = (
+#     data_clean
+#     .filter(pl.col("text").str.contains(pattern))
+#     .filter(~pl.col("text").str.contains("(?i)biodivers"))
+#     # .filter(~pl.col("text").str.contains("(?i)private equity"))
+# )
 
 print_data_info(data_keywords)
-# Number of rows: 30778
-# Number of unique urls: 13227
-# Number of unique domains: 589
-# Mean rows per url: 2.33
+# Number of rows: 12166
+# Number of unique urls: 5415
+# Number of unique domains: 547
+# Mean rows per url: 2.25
 
 data_keywords.write_csv("scraping/data_keywords.csv")
 
 
-urls_filtered = data_keywords["url"].unique()
+# add new column, containing the keywords, that are contained in the text column
+
+data_keywords = (
+    data_clean
+    # extract and mark all keywords included in the text elements 
+    .with_columns(
+        pl.col('text')
+        .str.to_lowercase()
+        .str.extract_all(pattern)
+        .alias('keywords')
+    )
+    # keep only rows (text elements) containing at least one keyword
+    # for controll: also previous and next text elements (add extra columns)
+    .with_columns(
+        pl.col('text').shift(1).over('url').alias('before_text'),      # Previous text
+        pl.col('text').shift(-1).over('url').alias('following_text'),   # Next text
+    )
+    .filter(pl.col('keywords').list.len() > 0)
+    .select(["country", "url", "keywords", "before_text", "text", "following_text"])
+)
+
+# write to excel file
+data_keywords.write_excel("topic_modeling/filtered_text_keywords.xlsx")
+
+# TODO: keep unique text elements per url 
+# (e.g. dislaimer text repeats per page, https://www.akkon-hochschule.de/stellenangebote)
+# Die Akkon Hochschule strebt an, die Diversität an der Hochschule zu erhöhen. Menschen mit Migrationsgeschichte und Rassismuserfahrungen sowie Frauen*, Trans* und nicht-binäre Personen und andere von intersektionaler Diskriminierung bedrohte oder be-troffene Personen werden ausdrücklich ermutigt, sich zu bewerben. Bewerbungen von Menschen mit Behinderung und ihnen Gleichgestellten werden bei vergleichbarer fachlicher und persönlicher Eignung bevorzugt berücksichtigt. Die Akkon Hochschule ist eine familienfreundliche Hochschule.
+
+
+df_keywords_per_country = (
+    data_keywords
+    .group_by('country')
+    .agg(
+        pl.concat_list('keywords').flatten().alias('keywords'),
+    )
+    .explode('keywords')
+    .group_by('country', 'keywords')
+    .agg(
+        pl.len().alias('count')
+    )
+    .sort('country', 'count', descending=True)
+)
+
+
+import matplotlib.pyplot as plt
+
+df_keyword_counts_pd = df_keywords_per_country.to_pandas()
+
+def plot_keyword_counts(df, cntry):
+    df_cntry = df[df['country'] == cntry].sort_values('count', ascending=True)
+
+    plt.figure(figsize=(6, 4))
+    plt.barh(y = df_cntry['keywords'], width = df_cntry['count'], color='skyblue')
+
+    plt.xlabel('Keywords')
+    plt.ylabel('Count')
+    plt.title(f'Keyword Counts for {cntry}')
+
+    plt.tight_layout()
+
+    plt.show()
+
+# Plot keyword counts for each URL
+for cntry in df_keyword_counts_pd['country'].unique():
+    plot_keyword_counts(df_keyword_counts_pd, cntry)
 
 
 # 2. keep all text elements from filteres urls (one part of page with diversity)
